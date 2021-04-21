52b3dc800c31c08076c1fc829524c40fe13a8577
jelle De Beukeleer <jelle.debeukeleer@student.uantwerpen.be>
2021-04-21 14:04:38 +0200

    fixed temporary text output of delay ratings, along with color

--- custom_measurements/measurements_handler.py
+++ custom_measurements/measurements_handler.py
@@ -4,6 +4,7 @@ from data_handler import DataHandler, rating_color
 from probe_lists import foreign_probes, local_probes
 
 current_dir = "./measurements/"
+output_dir = "./ratings/"
 files = [
     "chaos",
     "gaia",
@@ -39,7 +40,20 @@ if __name__ == "__main__":
         DH = DataHandler(ping_results, file)
         all_data[file] = DH
 
-    ratings = all_data["gaia"].generate_ratings_average_delay(foreign_probes)
-    for k in ratings.keys():
-        print(k, ":", ratings[k], ", ", rating_color(ratings[k]))
+    for server_name in files:
+        curr_local_probes = local_probes[server_name]
+        ratings = all_data[server_name].generate_ratings_average_delay(foreign_probes)
+        filename = output_dir + str(server_name) + ".txt"
+        of = open(filename, "w")
+        of.write("====== foreign probe ratings: ======\n")
+        for k in ratings.keys():
+            of.write(str(k) + " : " + str(ratings[k]) + ", " + str(rating_color(ratings[k])))
+            of.write("\n")
+
+        ratings = all_data[server_name].generate_ratings_average_delay(curr_local_probes)
+        of.write("\n====== local probe ratings: ======\n")
+        for k in ratings.keys():
+            of.write(str(k) + " : " + str(ratings[k]) + ", " + str(rating_color(ratings[k])))
+            of.write("\n")
+
     pass
